f511828587d52045e53800b401f93190fe27c82c
Sebastian Regucki <sebreg11@gmail.com>
2023-02-04 22:31:23 +0100

    [SB-1] spring-boot-base module creation

---
+++ libs/java/spring-boot-base/src/test/java/com/quantice/springbootbase/SpringBootBaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.quantice.springbootbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
