e010829116cd921c8abe97fbf937a70c603f1ef4
Sebastian Regucki <sebreg11@gmail.com>
2023-02-13 18:28:30 +0100

    [SB-8] User management service initialization

---
+++ apps/server/user-management-service/src/test/java/com/quantice/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.quantice.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
