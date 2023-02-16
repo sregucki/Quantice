c477aca1cb431856a998447b2bf6086083dafe8f
Sebastian Regucki <sebreg11@gmail.com>
2023-02-16 23:23:56 +0100

    [SB-13] Refactor

--- apps/server/user-management-service/src/main/java/com/quantice/usermanagementservice/model/converter/RoleListConverter.java
+++ apps/server/user-management-service/src/main/java/com/quantice/usermanagementservice/model/converter/RoleListConverter.java
@@ -1,6 +1,6 @@
 package com.quantice.usermanagementservice.model.converter;
 
-import com.quantice.usermanagementservice.model.enums.UserRole;
+import com.quantice.usermanagementservice.model.enums.RoleType;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
@@ -10,19 +10,19 @@ import java.util.List;
 import static java.util.Collections.emptyList;
 
 @Converter
-public class UserRoleListConverter implements AttributeConverter<List<UserRole>, String> {
+public class RoleListConverter implements AttributeConverter<List<RoleType>, String> {
 
     private static final String SPLIT_CHAR = ",";
 
     @Override
-    public String convertToDatabaseColumn(final List<UserRole> attribute) {
+    public String convertToDatabaseColumn(final List<RoleType> attribute) {
 
         return attribute != null ? String.join(SPLIT_CHAR, attribute.stream().map(Enum::name).toList()) : "";
     }
 
     @Override
-    public List<UserRole> convertToEntityAttribute(final String dbData) {
+    public List<RoleType> convertToEntityAttribute(final String dbData) {
 
-        return dbData != null ? Arrays.stream(dbData.split(SPLIT_CHAR)).map(UserRole::valueOf).toList() : emptyList();
+        return dbData != null ? Arrays.stream(dbData.split(SPLIT_CHAR)).map(RoleType::valueOf).toList() : emptyList();
     }
 }
