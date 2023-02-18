e60f98fc86582d3534a7ce93de0577d7b2fe750f
Sebastian Regucki <sebreg11@gmail.com>
2023-02-18 19:31:53 +0100

    [SB-13] Refactor

--- apps/server/authentication-service/src/main/resources/data.sql
+++ apps/server/authentication-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO auth_provider (auth_provider_id, auth_provider_name, auth_provider_type)
-VALUES (1, 'QUANTICE', 'INTERNAL'),
-       (2, 'GOOGLE', 'EXTERNAL')
+VALUES (1, 'quantice', 'INTERNAL'),
+       (2, 'google', 'EXTERNAL')
 ON CONFLICT DO NOTHING;
