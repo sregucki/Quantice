2bb7055f239a61de18c4711cbd0efbcb9ecf262c
Sebastian Regucki <sebreg11@gmail.com>
2023-02-26 22:27:14 +0100

    [SB-25] Util method for enum AuthProvider

--- apps/server/auth-service/src/main/java/com/quantice/authservice/model/enums/AuthProvider.java
+++ apps/server/auth-service/src/main/java/com/quantice/authservice/model/enums/AuthProvider.java
@@ -1,5 +1,10 @@
 package com.quantice.authservice.model.enums;
 
 public enum AuthProvider {
-    QUANTICE, GOOGLE
+    QUANTICE, GOOGLE;
+
+    public static AuthProvider valueOfIgnoreCase(String name) {
+
+        return valueOf(name.toUpperCase());
+    }
 }
