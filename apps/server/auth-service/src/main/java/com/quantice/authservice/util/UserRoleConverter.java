87715b9c7c09a5e41adee8fd8554bf4f21a2dc42
Sebastian Regucki <sebreg11@gmail.com>
2023-02-26 21:33:56 +0100

    [SB-25] User role converter

---
+++ apps/server/auth-service/src/main/java/com/quantice/authservice/util/UserRoleConverter.java
@@ -0,0 +1,28 @@
+package com.quantice.authservice.util;
+
+import com.quantice.authservice.model.enums.Role;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+@Converter
+public class UserRoleConverter implements AttributeConverter<List<Role>, String> {
+
+    private static final String SPLIT_CHAR = ",";
+
+    @Override
+    public String convertToDatabaseColumn(final List<Role> attribute) {
+
+        return attribute != null ? String.join(SPLIT_CHAR, attribute.stream().map(Enum::name).toList()) : "";
+    }
+
+    @Override
+    public List<Role> convertToEntityAttribute(final String dbData) {
+
+        return dbData != null ? Arrays.stream(dbData.split(SPLIT_CHAR)).map(Role::valueOf).toList() : emptyList();
+    }
+}
