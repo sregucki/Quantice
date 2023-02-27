ab7c24545ba55f8f13c7ab92093a3c3076019e8a
Sebastian Regucki <sebreg11@gmail.com>
2023-02-27 21:36:10 +0100

    [SB-25] schema.sql update

--- apps/server/auth-service/src/main/resources/schema.sql
+++ apps/server/auth-service/src/main/resources/schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS user_auth_data
 (
     id                 UUID PRIMARY KEY,
+    username           TEXT NOT NULL,
     email              TEXT NOT NULL UNIQUE,
     password_hash      TEXT,
+    image_url          TEXT,
     auth_provider      TEXT NOT NULL,
     user_role          TEXT NOT NULL
 );
