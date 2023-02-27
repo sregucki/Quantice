95309e56f0e8e9426e4fdc41f71c4d9475affc22
Sebastian Regucki <sebreg11@gmail.com>
2023-02-27 23:42:58 +0100

    [SB-25] login endpoint

--- apps/server/auth-service/src/main/java/com/quantice/authservice/controller/UserController.java
+++ apps/server/auth-service/src/main/java/com/quantice/authservice/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.quantice.authservice.controller;
 
 import com.quantice.authservice.model.User;
+import com.quantice.authservice.model.dto.LoginRequest;
 import com.quantice.authservice.model.dto.RegisterRequest;
 import com.quantice.authservice.model.enums.AuthProvider;
 import com.quantice.authservice.security.PasswordEncoder;
@@ -9,8 +10,12 @@ import com.quantice.authservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,10 +29,31 @@ public class UserController {
     private final TokenProvider tokenProvider;
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
 
     // TODO @Valid on request bodies
+    // TODO exception handling
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest loginRequest) {
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                loginRequest.getEmail(), loginRequest.getPassword()
+                )
+        );
+
+        String token = tokenProvider.createToken(tokenProvider.getName(authentication));
+
+        LOGGER.info(String.format("Token for logging user: %s", token));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+
+        return new ResponseEntity(headers, HttpStatus.OK);
+    }
+
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody RegisterRequest registerRequest) {
 
