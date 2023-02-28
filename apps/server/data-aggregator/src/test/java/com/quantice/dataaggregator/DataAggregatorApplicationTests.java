7f8668b391ccafbde2010f7dfc8b4549f46d3bb9
Sebastian Regucki <sebreg11@gmail.com>
2023-02-28 13:03:42 +0100

    [SB-27] data aggregator service init

---
+++ apps/server/data-aggregator/src/test/java/com/quantice/dataaggregator/DataAggregatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.quantice.dataaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
