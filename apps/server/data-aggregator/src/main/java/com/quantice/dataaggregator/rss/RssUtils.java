9ca5064a5c68b200d9b21914aad7009da8e90cc8
Sebastian Regucki <sebreg11@gmail.com>
2023-03-03 15:11:47 +0100

    [SB-27] Rss utils with impl

---
+++ apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/rss/RssUtils.java
@@ -0,0 +1,16 @@
+package com.quantice.dataaggregator.rss;
+
+import com.rometools.rome.feed.synd.SyndFeed;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public interface RssUtils {
+
+    boolean isActive(String url);
+
+    Optional<SyndFeed> getParsingResult(String url);
+
+    List<String> getChannels() throws IOException;
+
+}
