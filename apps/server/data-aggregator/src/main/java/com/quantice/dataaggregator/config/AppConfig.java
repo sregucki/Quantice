a51affdcf5ff34a2fa386dfb96c5a294cb52d857
Sebastian Regucki <sebreg11@gmail.com>
2023-03-03 15:11:20 +0100

    [SB-27] Config class with syndfeed bean

--- apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/config/AppConfig.java
+++ apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.quantice.dataaggregator.config;
 
+import com.rometools.rome.io.SyndFeedInput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class AppConfig {
+
+    @Bean
+    public SyndFeedInput syndFeedInput() {
+        return new SyndFeedInput();
+    }
 
 }
