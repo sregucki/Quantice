ab629a2c04db79371c394dbd0a660148ae7f3c40
Sebastian Regucki <sebreg11@gmail.com>
2023-03-03 19:27:05 +0100

    [SB-27] Better stream api method

--- apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/schedule/RssSchedule.java
+++ apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/schedule/RssSchedule.java
@@ -2,7 +2,6 @@ package com.quantice.dataaggregator.schedule;
 
 import com.quantice.dataaggregator.rss.RssChannelReader;
 import com.quantice.dataaggregator.rss.RssUtils;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,12 +15,13 @@ public class RssSchedule {
 
     /**
      * Fetch all data from rss after every 60 minutes
-     * @throws IOException
+     *
      */
     @Scheduled(fixedRate = 3600000)
-    public void aggregateRss() throws IOException {
-        rssUtils.getChannels().forEach(rssChannelReader::readChannel);
+    public void aggregateRss() {
+        rssUtils.getChannels().stream().map(
+            channel -> rssChannelReader.readChannel(channel).subscribe()
+        ).toList();
     }
-
 
 }
