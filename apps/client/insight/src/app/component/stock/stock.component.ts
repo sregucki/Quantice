4b39058dee9ec2bbe7ea231489f53dea01fd92bc
Sebastian Regucki <sebreg11@gmail.com>
2023-03-09 12:34:45 +0100

    [QC-2] Route call fix (call one time)

--- apps/client/insight/src/app/component/stock/stock.component.ts
+++ apps/client/insight/src/app/component/stock/stock.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {Location} from "@angular/common";
+import {ActivationEnd, Router} from "@angular/router";
+import {ArticleService} from "../../service/article.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-stock',
@@ -6,5 +10,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./stock.component.css']
 })
 export class StockComponent {
+  sub: Subscription;
+  stockName: string;
+
+  constructor(private location: Location, private router: Router, private articleService: ArticleService) {
+    this.sub = router.events.subscribe((val) => {
+      if (val instanceof ActivationEnd) {
+        if (location.path() != '') {
+          this.stockName = location.path().split("/")[2];
+        } else {
+          this.stockName = 'home' // TODO error here
+        }
+      }
+    });
+  }
 
 }
