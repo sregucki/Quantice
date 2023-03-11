e0666e5b85d7f9e5e152a46a71423ac6ac24a248
Sebastian Regucki <sebreg11@gmail.com>
2023-03-11 13:21:34 +0100

    [SB-30] stock api cors enabling

--- apps/server/stock-data-api/main.py
+++ apps/server/stock-data-api/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from stock_data_service import get_historical_stock_data
 
 app = FastAPI()
+
+# TODO enable only fixed origins
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/stock/data")
 async def root(ticker: str, period: str, interval: str):
     e = get_historical_stock_data(ticker, period, interval)
     return {"data": e.to_dict()}
-
