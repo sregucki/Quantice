a992e6f2fb67b1c448177e81ed5df4aa6abf24d4
Sebastian Regucki <sebreg11@gmail.com>
2023-03-11 22:41:48 +0100

    [QC-2] stock-chart-service setup

---
+++ apps/client/insight/src/app/service/stock-chart-service/stock-chart.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {StockChart} from "angular-highcharts";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StockChartService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getLineChart(ticker: string, data: unknown[][]): StockChart {
+    return new StockChart({
+      chart: {
+        backgroundColor: 'white'
+      },
+      rangeSelector: {
+        selected: 1
+      },
+      title: {
+        text: ticker
+      },
+      series: [{
+        type: 'line',
+        tooltip: {
+          valueDecimals: 2
+        },
+        name: ticker,
+        data: data
+      }]
+    })
+  }
+
+}
