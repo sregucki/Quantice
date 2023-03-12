cabb81f617a2a99308c7415846b2ccdca04e23f5
Sebastian Regucki <sebreg11@gmail.com>
2023-03-12 22:56:22 +0100

    [QC-3] graphql schema url depending on env

--- apps/client/insight/src/app/graphql.module.ts
+++ apps/client/insight/src/app/graphql.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {APOLLO_OPTIONS, ApolloModule} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
+import {environment} from "../environments/environment";
 
-const uri = 'http://localhost:8083/graphql'; // <-- add the URL of the GraphQL server here
+const uri = environment.graphQLSchemaUrl;
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
--- apps/client/insight/src/environments/environment.development.ts
+++ apps/client/insight/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  stockApiUrl: 'http://localhost:8087'
+  stockApiUrl: 'http://localhost:8087',
+  graphQLSchemaUrl: 'http://localhost:8083/graphql'
 };
