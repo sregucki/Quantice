1e2c9c508d6bb9902874457b3dfacb12ce1f79b2
Sebastian Regucki <sebreg11@gmail.com>
2023-03-14 16:09:29 +0100

    [SB-34] Text indexing

--- apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/model/Entry.java
+++ apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/model/Entry.java
@@ -9,7 +9,9 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.TextScore;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,9 +24,18 @@ public class Entry {
     @Id
     private String id;
     @Indexed(unique = true)
+    @TextIndexed(weight = 3)
     private String url;
+
+    @TextIndexed(weight = 5)
     private String title;
+
+    @TextIndexed(weight = 2)
     private String description;
+
     private Instant publishedAt;
+
+    @TextScore
+    private Float score;
 
 }
