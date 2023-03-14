02692e707994b754597bc283ead6d66a69f0c304
Sebastian Regucki <sebreg11@gmail.com>
2023-03-14 09:24:03 +0100

    [SB-32] Removal of Text indexing - moved directly to Insight

--- apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/model/Entry.java
+++ apps/server/data-aggregator/src/main/java/com/quantice/dataaggregator/model/Entry.java
@@ -9,9 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.index.TextIndexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.TextScore;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,14 +22,9 @@ public class Entry {
     @Id
     private String id;
     @Indexed(unique = true)
-    @TextIndexed
     private String url;
-    @TextIndexed
     private String title;
-    @TextIndexed
     private String description;
-    @TextScore
-    public Float score;
     private Instant publishedAt;
 
 }
