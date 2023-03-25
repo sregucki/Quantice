3208c326b2c60a57115ca676710edf8931cbfa67
Sebastian Regucki <sebreg11@gmail.com>
2023-03-25 15:14:15 +0100

    [QC-17] refactor

--- apps/client/app-gateway/src/app/app.module.ts
+++ apps/client/app-gateway/src/app/app.module.ts
@@ -4,7 +4,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './component/login/login.component';
-import {RegisterComponent} from './register/register.component';
+import {RegisterComponent} from './component/register/register.component';
 
 @NgModule({
   declarations: [
