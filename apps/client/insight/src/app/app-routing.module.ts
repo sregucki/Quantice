d8250eb364f0cb0b8dccca9874b85e54a5b8563c
Sebastian Regucki <sebreg11@gmail.com>
2023-03-26 00:17:07 +0100

    [QC-17] token auth

--- apps/client/insight/src/app/app-routing.module.ts
+++ apps/client/insight/src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from "./component/dashboard/dashboard.component";
 import {StockComponent} from "./component/stock/stock.component";
+import {AuthService} from "./service/auth-service/auth.service";
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthService] },
   { path: 'stock/:name', component: StockComponent }
 ];
 
---
+++ apps/client/insight/src/app/service/auth-service/auth.service.ts
@@ -0,0 +1,43 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {map, Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService implements CanActivate {
+
+  constructor(private http: HttpClient) { }
+
+  getTokenFromCookie(): string | null {
+    const tokenPrefix = 'quantice-';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    let cookie = decodedCookie.split(';').filter(cookie => cookie.includes(tokenPrefix));
+    return cookie.length != 0 ? cookie[0].substring(tokenPrefix.length + 1) : null;
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.getTokenFromCookie()) {
+      return this.http.post(environment.authServerUrl, {token: this.getTokenFromCookie()}).pipe(
+        map((response: any) => {
+          return response.valueOf();
+        })
+      );
+    }
+    else {
+      this.redirectToLoginPage();
+      return false;
+    }
+  }
+
+  redirectToLoginPage(): void {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', environment.loginPageUrl, true);
+    xhr.withCredentials = true;
+    xhr.send();
+    window.location.href = environment.loginPageUrl;
+  }
+
+}
