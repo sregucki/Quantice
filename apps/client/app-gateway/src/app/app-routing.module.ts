4da8bb1416b6df1481a92add1dd5fcb71545ff47
Sebastian Regucki <sebreg11@gmail.com>
2023-03-26 00:12:18 +0100

    [QC-17] routes to login and register pages

--- apps/client/app-gateway/src/app/app-routing.module.ts
+++ apps/client/app-gateway/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {RegisterComponent} from "./component/register/register.component";
+import {LoginComponent} from "./component/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: '**', redirectTo: 'login'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
