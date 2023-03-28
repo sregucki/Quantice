01f1e2d814adc6ca6a683a2d3b76e3f187bd1f47
Sebastian Regucki <sebreg11@gmail.com>
2023-03-28 23:02:12 +0200

    [QC-18] basic indexShowcase list

--- apps/client/insight/src/app/component/dashboard/dashboard.component.ts
+++ apps/client/insight/src/app/component/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, NgZone, OnInit} from '@angular/core';
 import {map, Observable, startWith} from "rxjs";
 import {FormControl} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import data from '../../../assets/data/stocks.json'
 import {Router} from "@angular/router";
+import {StockService} from "../../service/stock-service/stock.service";
 
 export interface Stock {
   id: string;
@@ -20,19 +21,44 @@ export interface Stock {
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent  {
+export class DashboardComponent implements OnInit {
 
   stateCtrl = new FormControl();
   filteredStocks: Observable<Stock[]>;
   stocks: Stock[] = data as Stock[];
+  indexShowcaseTickers: string[] = ['META', 'AMZN', 'AAPL', 'NFLX', "GOOGL", "NVDA", "TSLA", "XOM", "JNJ",
+  "JPM", "WMT", "CSCO", "PM", "AMD", "ADBE", "ORCL", "UPS"];
+  indexShowcasePriceData: Map<string, any> = new Map<string, any>();
 
-  constructor(private http: HttpClient, private router: Router) {
+  constructor(private http: HttpClient, private router: Router, private stockService: StockService, private ngZone: NgZone) {
     this.filteredStocks = this.stateCtrl.valueChanges.pipe(
       startWith(''),
       map(state => (state ? this._filterStates(state) : this.stocks.slice())),
     );
+
+
   }
 
+  ngOnInit(): void {
+    this.indexShowcaseTickers.forEach(ticker => {
+      this.stockService.getStockData(ticker, '1mo', '1d').subscribe(stockData => {
+        const closePrice = Object.values(stockData['data' as keyof typeof stockData]['Close' as keyof typeof stockData]);
+        this.indexShowcasePriceData.set(ticker, [closePrice[closePrice.length - 1], closePrice[closePrice.length - 2]]);
+      })
+    })
+  }
+
+  getIndexData(ticker: string): number[] {
+    return this.indexShowcasePriceData.get(ticker);
+  }
+
+  getPriceDiff(price1: any, price2: any): number {
+    return Number(price1) - Number(price2);
+  }
+
+  getPriceDiffPercent(price1: any, price2: any): number {
+    return (1 - (Number(price2) / Number(price1))) * 100;
+  }
 
   private _filterStates(value: string): Stock[] {
     const filterValue = value.toLowerCase();
@@ -58,8 +84,5 @@ export class DashboardComponent  {
       });
   }
 
-  test() {
-    console.log('test')
-  }
 
 }
