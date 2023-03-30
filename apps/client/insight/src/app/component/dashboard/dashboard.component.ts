7e44873185451af86857379175d8d07b2f28c899
Sebastian Regucki <sebreg11@gmail.com>
2023-03-30 18:56:04 +0200

    [QC-18] dashboard design

--- apps/client/insight/src/app/component/dashboard/dashboard.component.ts
+++ apps/client/insight/src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
-import {Component, NgZone, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {map, Observable, startWith} from "rxjs";
 import {FormControl} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import data from '../../../assets/data/stocks.json'
 import {Router} from "@angular/router";
 import {StockService} from "../../service/stock-service/stock.service";
+import {Headline, HeadlineService} from "../../service/headline-service/headline.service";
+import {StockIndex} from "../../model/stockIndex/stockIndex";
 
 export interface Stock {
   id: string;
@@ -23,41 +25,46 @@ export interface Stock {
 })
 export class DashboardComponent implements OnInit {
 
+  @ViewChild('headline-major-title') containerRef: ElementRef;
+
   stateCtrl = new FormControl();
   filteredStocks: Observable<Stock[]>;
   stocks: Stock[] = data as Stock[];
-  indexShowcaseTickers: string[] = ['META', 'AMZN', 'AAPL', 'NFLX', "GOOGL", "NVDA", "TSLA", "XOM", "JNJ",
-  "JPM", "WMT", "CSCO", "PM", "AMD", "ADBE", "ORCL", "UPS"];
-  indexShowcasePriceData: Map<string, any> = new Map<string, any>();
+  indexShowcaseTickers: string[] = ['META', 'AMZN', 'AAPL', 'NFLX', "GOOGL"];
+  indexShowcaseList: StockIndex[] = [];
+  headlines: Headline[];
 
-  constructor(private http: HttpClient, private router: Router, private stockService: StockService, private ngZone: NgZone) {
+  constructor(private http: HttpClient, private router: Router, private stockService: StockService, private headlineService: HeadlineService) {
     this.filteredStocks = this.stateCtrl.valueChanges.pipe(
       startWith(''),
       map(state => (state ? this._filterStates(state) : this.stocks.slice())),
     );
-
-
+    console.log(this.indexShowcaseList);
   }
 
   ngOnInit(): void {
     this.indexShowcaseTickers.forEach(ticker => {
       this.stockService.getStockData(ticker, '1mo', '1d').subscribe(stockData => {
         const closePrice = Object.values(stockData['data' as keyof typeof stockData]['Close' as keyof typeof stockData]);
-        this.indexShowcasePriceData.set(ticker, [closePrice[closePrice.length - 1], closePrice[closePrice.length - 2]]);
+        let index: StockIndex = new StockIndex(ticker, closePrice[closePrice.length - 1] as number, closePrice[closePrice.length - 2] as number);
+        this.indexShowcaseList.push(index);
       })
+    })
+    this.headlineService.getHeadlines().subscribe(v => {
+      this.headlines = v as Headline[];
     })
   }
 
-  getIndexData(ticker: string): number[] {
-    return this.indexShowcasePriceData.get(ticker);
+  isDown(stockIndex: StockIndex): boolean {
+    return this.getPriceDiff(stockIndex) > 0;
   }
 
-  getPriceDiff(price1: any, price2: any): number {
-    return Number(price1) - Number(price2);
+  getPriceDiff(stockIndex: StockIndex): number {
+    return stockIndex.closePriceCurrent - stockIndex.closePriceBefore;
   }
 
-  getPriceDiffPercent(price1: any, price2: any): number {
-    return (1 - (Number(price2) / Number(price1))) * 100;
+  getPriceDiffPercent(stockIndex: StockIndex): number {
+    return (1 - (stockIndex.closePriceBefore / stockIndex.closePriceCurrent)) * 100;
   }
 
   private _filterStates(value: string): Stock[] {
