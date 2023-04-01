1e7cc644dc4b3cfa3d1ebee83aed6672580df7b0
Sebastian Regucki <sebreg11@gmail.com>
2023-04-01 21:38:24 +0200

    [QC-18] refactor

--- apps/client/insight/src/app/component/stock/stock.component.ts
+++ apps/client/insight/src/app/component/stock/stock.component.ts
@@ -86,7 +86,7 @@ export class StockComponent implements OnInit, AfterViewInit {
         stockDataVolumeFormatted.push([Date.parse(Object.keys(stockDataOpen)[i]), Object.values(stockDataVolume)[i]])
       }
 
-      this.stockChart = this.stockChartService.getLineChart(this.ticker, stockDataAll, stockDataCloseFormatted, stockDataVolumeFormatted);
+      this.stockChart = this.stockChartService.getChartConsolidated(this.ticker, stockDataAll, stockDataCloseFormatted, stockDataVolumeFormatted);
       this.lastPrice = (
         Number(stockDataCloseFormatted[stockDataCloseFormatted.length - 1][1])
       );
