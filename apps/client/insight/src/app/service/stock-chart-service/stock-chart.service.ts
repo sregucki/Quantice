43693fb3efedd7d0966d4d5ba67a6a47b427d1ba
Sebastian Regucki <sebreg11@gmail.com>
2023-04-01 21:37:25 +0200

    [QC-18] line chart for dashboard

--- apps/client/insight/src/app/service/stock-chart-service/stock-chart.service.ts
+++ apps/client/insight/src/app/service/stock-chart-service/stock-chart.service.ts
@@ -14,7 +14,48 @@ export class StockChartService {
   constructor() {
   }
 
-  getLineChart(ticker: string, dataAll: any, dataClose: any, dataVolume: any): StockChart {
+  getLineChart(ticker: string, dataClose: any): StockChart {
+    return new StockChart({
+      chart: {
+        backgroundColor: 'white'
+      },
+      rangeSelector: {
+        enabled: false
+      },
+      navigator: {
+        enabled: false
+      },
+      scrollbar: {
+        enabled: false
+      },
+      title: {
+        useHTML: true,
+        text: `<span style='vertical-align: bottom; margin-right: 3px'>${ticker}</span>` +
+          `<img src='../../../assets/dashboard/${ticker}_logo.png' width='16' height='16' style='display: inline-block; vertical-align: top; margin-bottom: 2px'/>`,
+        style: {
+          fontSize: '14px'
+        }
+      },
+      yAxis: {
+        labels: {
+          format: '{value}$'
+        }
+      },
+      series: [{
+        id: 'line',
+        type: 'line',
+        tooltip: {
+          valueDecimals: 2
+        },
+        color:
+          '#3677a8',
+        name: ticker.toUpperCase(),
+        data: dataClose
+      }]
+    });
+  }
+
+  getChartConsolidated(ticker: string, dataAll: any, dataClose: any, dataVolume: any): StockChart {
 
     this.ticker = ticker
     this.dataAll = dataAll;
